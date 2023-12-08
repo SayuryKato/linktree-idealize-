895e652ce50b81373186916761140e3baf8f17a9
SayuryKato <skdp.ecp19@uea.edu.br>
2023-12-07 21:24:56 -0400

    :tada: Ultimas modificações Modo Dark

---
+++ script.js
@@ -0,0 +1,12 @@
+const mode = document.getElementById('modoDark');
+const botao = document.querySelectorAll('.fundo_botao');
+const iconeInstagram = document.querySelector('.bi bi-instagram');
+
+mode.addEventListener('click', () => {
+    const body = document.body;
+    body.classList.toggle('modo-noturno');
+    botao.forEach(element => {
+        element.classList.toggle('degrade'); 
+    });
+    iconeInstagram.classList.toggle('fundo_botao');
+});
