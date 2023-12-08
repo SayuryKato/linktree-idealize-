00d143ac35687ee2786d3499cbf044900b7227d4
SayuryKato <skdp.ecp19@uea.edu.br>
2023-12-07 21:34:38 -0400

    :tada: onclick

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const mode = document.getElementById('modoDark');
-const botao = document.querySelectorAll('.fundo_botao');
-const iconeInstagram = document.querySelector('.bi bi-instagram');
+// const mode = document.getElementById('modoDark');
+// const botao = document.querySelectorAll('.fundo_botao');
+// const iconeInstagram = document.querySelector('.bi bi-instagram');
 
-mode.addEventListener('click', () => {
+// mode.addEventListener('click', () => {
+//     const body = document.body;
+//     body.classList.toggle('modo-noturno');
+//     botao.forEach(element => {
+//         element.classList.toggle('degrade'); 
+//     });
+//     iconeInstagram.classList.toggle('fundo_botao');
+// });
+
+function cli_darkMode(){
+    const mode = document.getElementById('modoDark');
+    const botao = document.querySelectorAll('.fundo_botao');
+    const iconeInstagram = document.querySelector('.bi bi-instagram');
     const body = document.body;
     body.classList.toggle('modo-noturno');
     botao.forEach(element => {
         element.classList.toggle('degrade'); 
     });
     iconeInstagram.classList.toggle('fundo_botao');
-});
+}
